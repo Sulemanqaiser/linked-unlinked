3d9d1b6065c9f0088af5c3f12f865d424c76102d
Sulemanqaiser <sulemanqaiser2@gmail.com>
2020-09-05 20:45:09 +0500

    Update linked.js

--- linked.js
+++ linked.js
@@ -1,1 +1,9 @@
-// find all the users that have not been linked
+(all_users, linked_users) => {
+  
+ all_users.forEach(json_obj => {
+   if (linked_users.indexof(json_obj.id) == -1) {
+     new_array.push(json_obj)
+   }
+  }
+  return new_array
+}
